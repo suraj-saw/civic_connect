d79291cbb67b66e6fbe40a2c0c0d8de355221c92
suraj-saw <suraj03saw@gmail.com>
2026-04-03 00:20:53 +0530

    Checkpoint from VS Code for cloud agent session

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.civic_connect"
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
